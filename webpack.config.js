6d4407b95505f2fa31d78c0b4e1d483ec0d6003a
harwooddj <harwooddj@gmail.com>
2021-08-16 19:47:41 +0100

    bb

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,17 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: "/odinrestaurant/",
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
         type: 'asset/resource',
       },
     ],
